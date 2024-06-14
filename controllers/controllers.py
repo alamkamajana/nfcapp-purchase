5e6b46ea97afb0a0607211fade3010a15c391dd5
cerdasjanastu <cerdasjanastu@gmail.com>
2024-06-14 17:17:46 +0800

    update print controller

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -5,29 +5,45 @@ import random
 from werkzeug.utils import redirect
 from odoo import http
 from odoo.http import request, Response
+from typing import List, Union
 
 
 class Controller(http.Controller):
-    def compute_grand_total(self, po):
-        po_lines = request.env["nfcpurchase.purchase.order.line"].search([
-            ("purchase_order_uniq_id", "=", po.uniq_id)
-        ])
+    def compute_grand_total(self, po: 'nfcpurhcase.purchase.order'):
+        domain = [("purchase_order_uniq_id", "=", po.uniq_id)]
+        po_lines = request.env["nfcpurchase.purchase.order.line"].search(domain)
         grand_total = sum(line.subtotal for line in po_lines)
         return grand_total
 
 
-    def get_purchase_orders(self, pe_uniq_id):
-        purchase_orders = request.env["nfcpurchase.purchase.order"].search([
-            ("purchase_event_uniq_id", "=", pe_uniq_id)
-        ])
+    def get_purchase_orders(self, pe_uniq_id: str):
+        domain = [("purchase_event_uniq_id", "=", pe_uniq_id)]
+        purchase_orders = request.env["nfcpurchase.purchase.order"].search(domain)
         return purchase_orders
 
 
-    def get_purchase_order(self, po_uniq_id):
-        purchase_order = request.env["nfcpurchase.purchase.order"].search([
-            ("uniq_id", "=", po_uniq_id)
-        ], limit=1)
+    def get_purchase_order(self, po_uniq_id: str):
+        domain = [("uniq_id", "=", po_uniq_id)]
+        purchase_order = request.env["nfcpurchase.purchase.order"].search(domain, limit=1)
         return purchase_order
+
+
+    def get_purchase_order_lines(self, list_po_uniq_id: List[str]):
+        domain = [("purchase_order_uniq_id", "in", list_po_uniq_id)]
+        purchase_order_lines = request.env["nfcpurchase.purchase.order.line"].search(domain)
+        return purchase_order_lines
+
+
+    def get_delivery_orders(self, list_delivery_uniq_id: List[str]):
+        domain = [("uniq_id", "in", list_delivery_uniq_id)]
+        delivery_orders = request.env["nfcpurchase.delivery.order"].search(domain)
+        return delivery_orders
+
+
+    def get_money_entries(self, pe_uniq_id: str):
+        domain = [("purchase_event_uniq_id", "=", pe_uniq_id)]
+        money_entries = request.env["nfcpurchase.money"].search(domain)
+        return money_entries
 
 
     @http.route("/nfcpurchase/view/purchase-event/<int:po_id>", auth="user", type="http", website=True)
@@ -35,10 +51,7 @@ class Controller(http.Controller):
         if kw.get("pe_id"):
             pe_id = int(kw.get("pe_id"))
             purchase_event = request.env["nfcpurchase.purchase.event"].browse(pe_id)
-            pe_uniq_id = purchase_event.uniq_id
-            purchase_orders = request.env["nfcpurchase.purchase.order"].search([
-                ("purchase_event_uniq_id", "=", pe_uniq_id)
-            ])
+            purchase_orders = self.get_purchase_orders(purchase_event.uniq_id)
 
             return_values = {
                 "purchase_orders": purchase_orders,
@@ -53,24 +66,18 @@ class Controller(http.Controller):
         ])
         po_odoo = request.env["purchase.order"].browse(po_id)
 
-        def get_money_entries(pe_uniq_id):
-            money_entries = request.env["nfcpurchase.money"].search([
-                ("purchase_event_uniq_id", "=", pe_uniq_id)
-            ])
-            return money_entries
-
         def compute_money_in(pe_uniq_id):
-            money_entries = get_money_entries(pe_uniq_id)
+            money_entries = self.get_money_entries(pe_uniq_id)
             money_out = sum(money.amount for money in money_entries if money.amount < 0)
             return money_out
 
         def compute_money_out(pe_uniq_id):
-            money_entries = get_money_entries(pe_uniq_id)
+            money_entries = self.get_money_entries(pe_uniq_id)
             money_in = sum(money.amount for money in money_entries if money.amount > 0)
             return money_in
 
         def compute_balance(pe_uniq_id):
-            money_entries = get_money_entries(pe_uniq_id)
+            money_entries = self.get_money_entries(pe_uniq_id)
             total_fund = sum(money.amount for money in money_entries)
             return total_fund
 
@@ -82,7 +89,7 @@ class Controller(http.Controller):
             "compute_balance": compute_balance,
             "get_purchase_orders": self.get_purchase_orders,
             "compute_grand_total": self.compute_grand_total,
-            "get_money_entries": get_money_entries,
+            "get_money_entries": self.get_money_entries,
             "get_purchase_order": self.get_purchase_order,
         }
         return request.render("nfcapp-purchase.web_nfcpurchase_purchase_event", return_values)
@@ -95,9 +102,7 @@ class Controller(http.Controller):
         purchase_event = request.env["nfcpurchase.purchase.event"].search([
             ("uniq_id", "=", purchase_order.purchase_event_uniq_id)
         ], limit=1)
-        purchase_order_lines = request.env["nfcpurchase.purchase.order.line"].search([
-            ("purchase_order_uniq_id", "=", purchase_order.uniq_id)
-        ])
+        purchase_order_lines = self.get_purchase_order_lines([purchase_order.uniq_id])
         money_entries = request.env["nfcpurchase.money"].search([
             ("purchase_order_uniq_id", "=", purchase_order.uniq_id)
         ])
@@ -116,15 +121,9 @@ class Controller(http.Controller):
     @http.route("/nfcpurchase/view/delivery-order/<int:pe_id>", auth="user", type="http", website=True)
     def delivery_order_view(self, pe_id, **kw):
         purchase_event = request.env["nfcpurchase.purchase.event"].browse(pe_id)
-        purchase_orders = request.env["nfcpurchase.purchase.order"].search([
-            ("purchase_event_uniq_id", "=", purchase_event.uniq_id)
-        ])
-        purchase_order_lines = request.env["nfcpurchase.purchase.order.line"].search([
-            ("purchase_order_uniq_id", "in", purchase_orders.mapped("uniq_id"))
-        ])
-        delivery_orders = request.env["nfcpurchase.delivery.order"].search([
-            ("uniq_id", "in", purchase_order_lines.mapped("delivery_order_uniq_id"))
-        ])
+        purchase_orders = self.get_purchase_orders(purchase_event.uniq_id)
+        purchase_order_lines = self.get_purchase_order_lines(purchase_orders.mapped("uniq_id"))
+        delivery_orders = self.get_delivery_orders(purchase_order_lines.mapped("delivery_order_uniq_id"))
 
         def get_order_lines(delivery_uniq_id):
             order_lines = request.env["nfcpurchase.purchase.order.line"].search([
@@ -140,4 +139,42 @@ class Controller(http.Controller):
         }
 
         return request.render("nfcapp-purchase.web_nfcpurchase_delivery_order", return_values)
+
+
+    @http.route("/nfcpurchase/view/purchase-event/print/<int:pe_id>", auth="user", type="http", website=True)
+    def print_purchase_order_lines(self, pe_id, **kw):
+        purchase_event = request.env["nfcpurchase.purchase.event"].browse(pe_id)
+        purchase_orders = self.get_purchase_orders(purchase_event.uniq_id)
+        purchase_order_lines = self.get_purchase_order_lines(purchase_orders.mapped("uniq_id"))
+        return_values = {
+            "purchase_order_lines": purchase_order_lines
+        }
+
+        return request.render("nfcapp-purchase.web_nfcpurchase_print_purchase_event", return_values)
+
+
+    @http.route("/nfcpurchase/view/purchase-event/print-payments/<int:pe_id>", auth="user", type="http", website=True)
+    def print_payments(self, pe_id, **kw):
+        purchase_event = request.env["nfcpurchase.purchase.event"].browse(pe_id)
+        money_entries = self.get_money_entries(purchase_event.uniq_id)
+        return_values = {
+            "money_entries": money_entries,
+            "get_purchase_order": self.get_purchase_order,
+        }
+
+        return request.render("nfcapp-purchase.web_nfcpurchase_print_payments_purchase_event", return_values)
+
+
+    @http.route("/nfcpurchase/view/purchase-event/print-delivery/<int:pe_id>", auth="user", type="http", website=True)
+    def print_delivery(self, pe_id, **kw):
+        purchase_event = request.env["nfcpurchase.purchase.event"].browse(pe_id)
+        purchase_orders = self.get_purchase_orders(purchase_event.uniq_id)
+        purchase_order_lines = self.get_purchase_order_lines(purchase_orders.mapped("uniq_id"))
+        scanned_delivery_orders = purchase_order_lines.filtered(lambda line: line.delivery_order_id)
+        delivery_orders = self.get_delivery_orders(scanned_delivery_orders.mapped("delivery_order_uniq_id"))
+        return_values = {
+            "delivery_orders": delivery_orders,
+        }
+
+        return request.render("nfcapp-purchase.web_nfcpurchase_print_delivery_purchase_event", return_values)
 
